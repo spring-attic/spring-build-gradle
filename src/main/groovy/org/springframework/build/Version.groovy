70cb55ebdbecd57b0618aa2b9a733ce86b98e25e
Chris Beams <cbeams@vmware.com>
2010-11-09 15:43:08 -0800

    Refactor Version to a dedicated class

--- src/main/groovy/org/springframework/build/Version.groovy
+++ src/main/groovy/org/springframework/build/Version.groovy
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2002-2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.build
+
+import org.gradle.api.InvalidUserDataException
+
+
 class Version {
     /**
      * Indicates whether a version is a release, milestone, or snapshot
@@ -58,26 +79,3 @@ class Version {
                 "but got (" + version + ")");
     }
 }
-
-project.createVersion = { value ->
-    new Version(value)
-}
-
-def requiredPropSets = []
-
-gradle.taskGraph.whenReady { graph ->
-    requiredPropSets.each { args ->
-        if (graph.hasTask(args.taskPath)) {
-            def missingProps = args.requiredProps.findAll { prop ->
-                !project.hasProperty(prop)
-            }
-            if (missingProps) {
-                throw new GradleException("For executing the ${args.taskPath} task you need to set all ${missingProps} properties")
-            }
-        }
-    }
-}
-project.checkForProps = { Map args ->
-    requiredPropSets.add args
-}
-
